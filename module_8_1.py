49c7bf5eed31b003f7761d2699f7fd25998c90b8
livintin <natig.abdulov@gmail.com>
2024-10-03 16:35:02 +0700

    Add files via upload

---
+++ module_8_1.py
@@ -0,0 +1,17 @@
+try:
+    def add_everything_up(a, b):
+        if isinstance(a, (int, float)) and isinstance(b, (int, float)):
+            return a + b
+        elif isinstance(a, str) and isinstance(b, str):
+            return a + b
+        else:
+            return str(a) + str(b)
+
+    print(add_everything_up(123.456, 'строка'))
+    print(add_everything_up('яблоко', 4215))
+    print(add_everything_up(123.456, 7))
+
+except TypeError as exc:
+    print('Error:', exc)
+
+
